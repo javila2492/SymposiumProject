041316fc5d2425151cf4eee90d631536419fbcc5
BT_1N3_02 <e>
2019-02-13 10:12:01 -0500

    A

---
+++ src/scenes/GUIController.java
@@ -0,0 +1,14 @@
+package scenes;
+
+import javafx.scene.image.Image;
+
+public class GUIController
+{
+    private Image icon;
+
+
+    public void updateIcon()
+    {
+        Image a = characters.Character.getCurrentHealthIndicator();
+    }
+}
