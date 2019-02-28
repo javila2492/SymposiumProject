96e2849df5afab952ed515c71febf5bab3771706
rchu6120 <rchu6120@bths.edu>
2019-02-28 10:09:09 -0500

    Fixed character selection error.

--- src/scenes/CharSelectionController.java
+++ src/scenes/CharSelectionController.java
@@ -2,6 +2,7 @@ package scenes;
 
 import characters.*;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -17,6 +18,8 @@ public class CharSelectionController
     private Label descshower;
     @FXML
     private Label abilityshower;
+    @FXML
+    Button contbutton;
 
     public void charInfoChangerSabrina(MouseEvent mouseEvent)
     {
@@ -24,6 +27,7 @@ public class CharSelectionController
         statshower.setText("STATS: " + "\n" + sabrina.statBuilderShower());
         descshower.setText("DESCRIPTION: " + sabrina.getCharacterDesc());
         abilityshower.setText(sabrina.getAbilityDesc());
+        contbutton.setStyle("visibility: visible");
     }
 
     public void charInfoChangerVin(MouseEvent mouseEvent)
@@ -32,6 +36,7 @@ public class CharSelectionController
         statshower.setText("STATS: " + "\n" + vin.statBuilderShower());
         descshower.setText("DESCRIPTION: " + vin.getCharacterDesc());
         abilityshower.setText(vin.getAbilityDesc());
+        contbutton.setVisible(true);
     }
 
     public void charInfoChangerMikey(MouseEvent mouseEvent)
@@ -40,6 +45,7 @@ public class CharSelectionController
         statshower.setText("STATS: " + "\n" + mikey.statBuilderShower());
         descshower.setText("DESCRIPTION: " + mikey.getCharacterDesc());
         abilityshower.setText(mikey.getAbilityDesc());
+        contbutton.setVisible(true);
     }
 
     public void charInfoChangerJoey(MouseEvent mouseEvent)
@@ -48,6 +54,7 @@ public class CharSelectionController
         statshower.setText("STATS: " + "\n" + joey.statBuilderShower());
         descshower.setText("DESCRIPTION: " + joey.getCharacterDesc());
         abilityshower.setText(joey.getAbilityDesc());
+        contbutton.setVisible(true);
     }
 
     public void charInfoChangerFred(MouseEvent mouseEvent)
@@ -56,5 +63,6 @@ public class CharSelectionController
         statshower.setText("STATS: " + "\n" + fred.statBuilderShower());
         descshower.setText("DESCRIPTION: " + fred.getCharacterDesc());
         abilityshower.setText(fred.getAbilityDesc());
+        contbutton.setVisible(true);
     }
 }
